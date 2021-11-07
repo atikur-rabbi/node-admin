ad6f75c53221ce97be69361d21d18854def265a3
atikur-rabbi <msawon94@gmail.com>
2021-11-07 20:37:08 +0600

    vercel

--- config.js
+++ config.js
@@ -4,7 +4,6 @@ const assert = require("assert");
 const { mongodb } = process.env;
 assert(mongodb, "Database is required");
 
-
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || "127.0.0.1";
 
